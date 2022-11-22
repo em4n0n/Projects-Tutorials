ae3671f1895fb70d3e7cf0d286c28d06b200bcb1
em4n0n <58372945+em4n0n@users.noreply.github.com>
2022-11-21 21:47:49 -0800

    instantiate a custom object with a class

---
+++ LPTHW/exercise_11.py
@@ -0,0 +1,9 @@
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* catnip
+\t* grass
+"""
+
+print(fat_cat)
--- MM&Dunder.py
+++ MM&Dunder.py
@@ -7,6 +7,9 @@ class Vector:
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
+    def __repr__(self):
+        return f"X: {self.x}, Y: {self.y}"
+
 v1 = Vector(10, 20)
 v2 = Vector(50, 60)
 v3 = v1 + v2
