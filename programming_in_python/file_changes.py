eb043397e8a3d197236e0e42376134039d490603
em4n0n <58372945+em4n0n@users.noreply.github.com>
2023-01-19 22:37:03 -0800

    linspace

---
+++ programming_in_python/file_changes.py
@@ -0,0 +1,6 @@
+import os
+
+def print_changes():
+    contents = os.listdir(r'C:\Users\dmsim\OneDrive\Documents\GitHub\Tutorials\programming_in_python')
+    print("Current directory contents:")
+    print(contents)
